9b86c341fdad8e6e7c9db71bea5f76fa3701082b
Amine Kharbouch <akharbouch@SMB.SQLI.COM>
2022-03-14 14:39:19 +0100

    gitignore and animal class added

---
+++ Animal.java
@@ -0,0 +1,13 @@
+class Animal {
+
+    public static String getDefaultGreeting() {
+        return "hello!";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getDefaultGreeting());
+
+    }
+
+}
